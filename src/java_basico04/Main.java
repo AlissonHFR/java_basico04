1a38e83079b2f18fc814ea39a3798c6eb75ff130
Alisson <alissonhenrique98@gmail.com>
2020-03-04 20:15:19 -0300

    primeiro commit

---
+++ src/java_basico04/Main.java
@@ -0,0 +1,28 @@
+package java_basico04;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Main app = new Main();
+		app.start();
+		
+
+	}
+
+	private void start() {
+		
+		int i = 1, k = 1, j;
+		
+		while(i <=20) {
+			System.out.println(i);
+			i++;
+		}
+		
+		do {
+			System.out.println(k);
+		}while(k <= 20);
+		
+		for(j=)
+	}
+
+}
